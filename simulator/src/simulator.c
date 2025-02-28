07df4bc9a01d55b1b32ae0561882d94336e1078f
ManogyaDahal <bat16692@gmail.com>
2025-02-28 17:20:31 +0545

    Added Queue and Queue operations to the code

--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_VEHICLES 100
 
 const int SCREEN_WIDTH = 600;
 const int SCREEN_HEIGHT = 600;
@@ -22,6 +23,49 @@ typedef struct{
   int targetLane; 
 } Vehicle;
 
+typedef struct {
+    Vehicle *vehicles[MAX_VEHICLES];
+    int front;
+    int rear;
+    int size;
+} VehicleQueue;
+
+void initQueue(VehicleQueue *q) {
+    q->front = 0;
+    q->rear = -1;
+    q->size = 0;
+}
+
+int isQueueFull(VehicleQueue *q) {
+    return q->size >= MAX_VEHICLES;
+}
+
+int isQueueEmpty(VehicleQueue *q) {
+    return q->size == 0;
+}
+
+void enqueue(VehicleQueue *q, Vehicle *v) {
+    if (isQueueFull(q)) {
+        printf("Queue is full! Cannot enqueue vehicle %d\n", v->vehicle_id);
+        free(v);
+        return;
+    }
+    q->rear = (q->rear + 1) % MAX_VEHICLES;
+    q->vehicles[q->rear] = v;
+    q->size++;
+    printf("Enqueued vehicle %d on Road %c Lane %d\n", v->vehicle_id, v->road_id, v->lane);
+}
+
+Vehicle* dequeue(VehicleQueue *q) {
+    if (isQueueEmpty(q)) {
+        printf("Queue is empty!\n");
+        return NULL;
+    }
+    Vehicle *v = q->vehicles[q->front];
+    q->front = (q->front + 1) % MAX_VEHICLES;
+    q->size--;
+    return v;
+}
 int InitializeSDL(void) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
