7083017b3dc7ed1f18f1eb16404e01e9818274bc
ManogyaDahal <bat16692@gmail.com>
2025-03-01 11:20:57 +0545

    Simple Ui Adjustments

--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -123,34 +123,25 @@ void DrawDashedLine(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int 
 
 
 void DrawLaneMarking(SDL_Renderer *renderer){
-    SDL_Color laneMarking = {247, 233, 23, 255};
-    SDL_Color laneMarkingRed = {247, 0, 0, 255};
+    SDL_Color laneMarking = {192, 192, 192, 255}; 
     
     SDL_SetRenderDrawColor(renderer, laneMarking.r, laneMarking.g, laneMarking.b, laneMarking.a);
-    // Horizontal road markings
     DrawDashedLine(renderer, 0, 250, 150, 250, 10);
     DrawDashedLine(renderer, 0, 350, 150, 350, 10);
     DrawDashedLine(renderer, 450, 250, 600, 250, 10);
     DrawDashedLine(renderer, 450, 350, 600, 350, 10);
 
-    SDL_SetRenderDrawColor(renderer, laneMarkingRed.r, laneMarkingRed.g, laneMarkingRed.b, laneMarkingRed.a);
-    // Split middle lane markings - horizontal
-    DrawDashedLine(renderer, 600, 300, 450, 300, 0);  // Middle divider
-    DrawDashedLine(renderer, 0, 300, 150, 300, 0);  // Middle divider
+    DrawDashedLine(renderer, 600, 300, 450, 300, 0);  
+    DrawDashedLine(renderer, 0, 300, 150, 300, 0);    
 
-    SDL_SetRenderDrawColor(renderer, laneMarking.r, laneMarking.g, laneMarking.b, laneMarking.a);
-    // Vertical road markings
     DrawDashedLine(renderer, 250, 0, 250, 150, 10);
     DrawDashedLine(renderer, 350, 0, 350, 150, 10);
     DrawDashedLine(renderer, 250, 450, 250, 600, 10);
     DrawDashedLine(renderer, 350, 450, 350, 600, 10);
 
-    SDL_SetRenderDrawColor(renderer, laneMarkingRed.r, laneMarkingRed.g, laneMarkingRed.b, laneMarkingRed.a);
-    // Split middle lane markings - vertical
-    DrawDashedLine(renderer, 300, 0, 300, 150, 0);  // Middle divider
-    DrawDashedLine(renderer, 300, 600, 300, 450, 0);    // Middle divider
+    DrawDashedLine(renderer, 300, 0, 300, 150, 0);    
+    DrawDashedLine(renderer, 300, 600, 300, 450, 0);  
 }
-
 
 void DrawTrafficLight(SDL_Renderer *renderer, int XPos, int YPos, int isGreen, char *orientation) {
 
@@ -166,9 +157,9 @@ void DrawTrafficLight(SDL_Renderer *renderer, int XPos, int YPos, int isGreen, c
   SDL_Rect trafficLightRect;
 
   if (strcmp(orientation, "vertical") == 0) {
-    trafficLightRect = (SDL_Rect){XPos, YPos, width, height}; // Horizontal orientation
+    trafficLightRect = (SDL_Rect){XPos, YPos, width, height}; 
   } else if (strcmp(orientation, "horizontal") == 0) {
-    trafficLightRect = (SDL_Rect){XPos, YPos, height, width}; // Vertical orientation
+    trafficLightRect = (SDL_Rect){XPos, YPos, height, width}; 
   } else {
     printf("Invalid orientation: %s\n", orientation);
     return;
@@ -181,36 +172,28 @@ void TrafficLightState(SDL_Renderer *renderer, int nsGreen , int ewGreen) {
 
   northSouthGreen = nsGreen; 
   eastWestGreen = ewGreen; 
-    // Vertical lights control North-South traffic
+
     DrawTrafficLight(renderer, 175, 255, northSouthGreen, "vertical"); // North-South left lane
     DrawTrafficLight(renderer, 395, 255, northSouthGreen, "vertical"); // North-South right lane
 
-    // Horizontal lights control East-West traffic
     DrawTrafficLight(renderer, 255, 175, eastWestGreen, "horizontal"); // East-West upper lane
     DrawTrafficLight(renderer, 255, 395, eastWestGreen, "horizontal"); // East-West lower lane
 }
 
 void DrawBackground(SDL_Renderer *renderer) {
-    // Set background color (green for grass)
-    SDL_SetRenderDrawColor(renderer, 34, 139, 34, 255);
+    SDL_SetRenderDrawColor(renderer, 85, 107, 47, 255);
     SDL_RenderClear(renderer);
 
-    // Set road color (gray)
-    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+    SDL_SetRenderDrawColor(renderer, 64, 64, 64, 255);
 
-    // Horizontal road
     SDL_Rect horizontalRoad = {0, 150, 600, 300}; 
     SDL_RenderFillRect(renderer, &horizontalRoad);
 
-    // Vertical road
     SDL_Rect verticalRoad = {150, 0, 300, 600}; 
     SDL_RenderFillRect(renderer, &verticalRoad);
 
-    // Dashed lines for lane markings (yellow)
-  DrawLaneMarking(renderer); 
-
-    // Traffic lights
-  TrafficLightState(renderer,northSouthGreen,eastWestGreen); 
+    DrawLaneMarking(renderer); 
+    TrafficLightState(renderer, northSouthGreen, eastWestGreen); 
 }
 
 
@@ -220,29 +203,16 @@ typedef struct {
 } LanePosition;
 
 
-// Updated lanePositions array with corrected middle lane splits
 LanePosition lanePositions[4][3] = {
-    // A road lanes (North to South) (A1, A2, A3)
-    // A2 is split into two - leftmost (outgoing), rightmost (incoming)
     { {150, 250, -30, -30}, {270, 300, -30, -30}, {350, 450, -30, -30} },
-    
-    // B road lanes (South to North) (B1, B2, B3)
-    // B2 is split into two - leftmost (incoming), rightmost (outgoing)
     { {350, 450, 630, 630}, {300, 330, 630, 630}, {150, 250, 630, 630} },
-    
-    // C road lanes (East to West) (C1, C2, C3)
-    // C2 is split into two - uppermost (outgoing), lowermost (incoming)
     { {630, 630, 150, 250}, {630, 630, 270, 300}, {630, 630, 350, 450} },
-    
-    // D road lanes (West to East) (D1, D2, D3)
-    // D2 is split into two - uppermost (incoming), lowermost (outgoing)
     { {-30, -30, 350, 450}, {-30, -30, 300, 330}, {-30, -30, 150, 250} }
 };
 
 void getLaneCenter(char road, int lane, int *x, int *y) {
-    int roadIndex = road - 'A';  // Convert 'A'-'D' to index 0-3
-    int laneIndex = lane - 1;    // Convert 1-3 to index 0-2
-
+    int roadIndex = road - 'A';
+    int laneIndex = lane - 1;   
     int middleLaneOffset = 0;
     if (lane == 2) {
         if (road == 'A') {
@@ -267,50 +237,6 @@ void getLaneCenter(char road, int lane, int *x, int *y) {
     printf("Road: %c, Lane: %d, X: %d, Y: %d, Offset: %d\n", road, lane, *x, *y, middleLaneOffset);
 }
 
-// void getLaneCenter(char road, int lane, int *x, int *y) {
-//     int roadIndex = road - 'A';  // Convert 'A'-'D' to index 0-3
-//     int laneIndex = lane - 1;    // Convert 1-3 to index 0-2
-//     // Adjust middle lane (lane 2) position based on direction
-//     int middleLaneOffset = 0;
-//     if (lane == 2) {
-//         // For vertical roads
-//         if (road == 'A') {
-//             middleLaneOffset = -15; // Move left for outgoing
-//         } if (road == 'B') {
-//             middleLaneOffset = 15;  // Move right for incoming
-//         }
-//         // For horizontal roads
-//         if (road == 'D') {
-//             middleLaneOffset = 15;  // Move down for outgoing
-//         } if (road == 'C') {
-//             middleLaneOffset = -15; // Move up for incoming
-//         }
-//
-//     }
-//     
-//     // For horizontal roads (D and C)
-//     if (road == 'D') {
-//         *x = -30;  // Start beyond left edge
-//         *y = (lanePositions[roadIndex][laneIndex].y_start + lanePositions[roadIndex][laneIndex].y_end) / 2 - 20 / 2 + middleLaneOffset;
-//     }
-//     if (road == 'C') {
-//         *x = SCREEN_WIDTH + 10;  // Start beyond right edge
-//         *y = (lanePositions[roadIndex][laneIndex].y_start + lanePositions[roadIndex][laneIndex].y_end) / 2 - 20 / 2 + middleLaneOffset;
-//     }
-//     // For vertical roads (A and B)
-//     if (road == 'A') {
-//
-//         *x = (lanePositions[roadIndex][laneIndex].x_start + lanePositions[roadIndex][laneIndex].x_end) / 2 - 20 / 2 + middleLaneOffset;
-//         *y = -30;  // Start beyond top edge
-//     }
-//     if (road == 'B') {
-//         *x = (lanePositions[roadIndex][laneIndex].x_start + lanePositions[roadIndex][laneIndex].x_end) / 2 - 20 / 2 + middleLaneOffset;
-//         *y = SCREEN_HEIGHT + 10;  // Start beyond bottom edge
-//     }
-//
-//     printf("Road: %c, Lane: %d, Original X: %d, Original Y: %d, Offset: %d\n", road, lane, *x, *y, middleLaneOffset);
-// }
-
 Uint32 lastSwitchTime = 0;
 
 void updateTrafficLights() {
@@ -324,8 +250,8 @@ void updateTrafficLights() {
 }
 
 void drawVehicle(SDL_Renderer *renderer, Vehicle *vehicle) {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Red vehicle
-  SDL_RenderFillRect(renderer, &vehicle->rect);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  
+    SDL_RenderFillRect(renderer, &vehicle->rect);
 }
 
 static SDL_Window *window = NULL;
@@ -358,12 +284,10 @@ void moveVehicle(Vehicle *vehicle) {
             return;
         }
     }
-  /* Vehicles Stopping Logic */
   int shouldStop = 0;
   int stopX = vehicle->rect.x;
   int stopY = vehicle->rect.y;
 
-  // For lane 2 only
   if (vehicle->lane == 2) {
     if (vehicle->road_id == 'A' && northSouthGreen) {
       stopY = 150 - 20;
@@ -413,17 +337,14 @@ void moveVehicle(Vehicle *vehicle) {
     int reachedX = (abs(vehicle->rect.x - targetX) <= vehicle->speed);
     int reachedY = (abs(vehicle->rect.y - targetY) <= vehicle->speed);
 
-    // Prioritize movement direction based on road layout
     if ((vehicle->road_id == 'A' && vehicle->targetRoad == 'C') || 
         (vehicle->road_id == 'B' && vehicle->targetRoad == 'D')) {
-        // Move Y first
         if (!reachedY) {
             vehicle->rect.y += (vehicle->rect.y < targetY) ? vehicle->speed : -vehicle->speed;
         } else if (!reachedX) {
             vehicle->rect.x += (vehicle->rect.x < targetX) ? vehicle->speed : -vehicle->speed;
         }
     } else {
-        // Move X first
         if (!reachedX) {
             vehicle->rect.x += (vehicle->rect.x < targetX) ? vehicle->speed : -vehicle->speed;
         } else if (!reachedY) {
@@ -431,7 +352,6 @@ void moveVehicle(Vehicle *vehicle) {
         }
     }
 
-    // Snap to target position
     if (reachedX) vehicle->rect.x = targetX;
     if (reachedY) vehicle->rect.y = targetY;
 
@@ -439,14 +359,11 @@ void moveVehicle(Vehicle *vehicle) {
         vehicle->road_id = vehicle->targetRoad;
         vehicle->lane = vehicle->targetLane;
     }
-    // Debugging Output
     printf("Vehicle %d Position: (%d, %d) Target: (%d, %d)\n", 
             vehicle->vehicle_id, vehicle->rect.x, vehicle->rect.y, targetX, targetY);
 }
 
 int main() {
-  // Socket related code commented out during the development of UI elements
-  // int sock = create_socket();
 
   if (InitializeSDL() < 0) {
     return 1;
@@ -578,10 +495,6 @@ int main() {
   for (int i = 0; i < num_active_vehicles; i++) {
     if (active_vehicles[i]) free(active_vehicles[i]);
   }
-  // receive_data(sock);
-
-  // Close socket
-  // close(sock);
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
